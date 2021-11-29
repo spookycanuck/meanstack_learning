4c29ab77a8716a2c0358a36b2e129ac901ddefd5
EM <eric.mann2393@gmail.com>
2021-11-29 15:11:33 -0500

    connected DB, add post/fetch data! took all fucking morning. fuck

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,34 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-const Post = require('./models/post');
+const Post = require("./models/post");
 
 const app = express();
+
+mongoose
+  .connect(
+    "mongodb://test:testpassword123@meanstack-shard-00-00.ej768.mongodb.net:27017,meanstack-shard-00-01.ej768.mongodb.net:27017,meanstack-shard-00-02.ej768.mongodb.net:27017/meanstack?ssl=true&replicaSet=atlas-erm6yf-shard-0&authSource=admin&retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Connected to database!");
+  })
+  .catch((err) => {
+    console.log("Connection failed!\n" + err);
+  });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
   );
-  res.setHeader('Access-Control-Allow-Methods',
-    'GET, POST, PATCH, DELETE, OPTIONS'
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS"
   );
   next();
 });
@@ -25,33 +38,18 @@ app.post("/api/posts", (req, res, next) => {
     title: req.body.title,
     content: req.body.content,
   });
-  console.log(post);
+  post.save();
   res.status(201).json({
-    message: 'Post added successfully'
+    message: "Post added successfully",
   });
 });
 
 app.get("/api/posts", (req, res, next) => {
-  const posts = [
-    {
-      id: "fasdf",
-      title: "first server-side post",
-      content: "this post is coming from the server",
-    },
-    {
-      id: "fasdfasd",
-      title: "second server-side post",
-      content: "this post is coming from the server!",
-    },
-    {
-      id: "fasdffd",
-      title: "third server-side post",
-      content: "this post is coming from the server?!",
-    },
-  ];
-  res.status(200).json({
-    message: 'Posts fetched successfully!',
-    posts: posts
+  Post.find().then((documents) => {
+    res.status(200).json({
+      message: "Posts fetched successfully!",
+      posts: documents,
+    });
   });
 });
 
