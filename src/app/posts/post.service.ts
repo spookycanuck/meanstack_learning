8677180460a974d5ad4f893b09c4e8972d71f3a8
EM <eric.mann2393@gmail.com>
2021-11-29 15:19:33 -0500

    correct _id/id issue from DB to front end

--- src/app/posts/post.service.ts
+++ src/app/posts/post.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { Post } from './post.model';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class PostsService {
   private posts: Post[] = [];
   private postUpdated = new Subject<Post[]>();
@@ -12,9 +13,21 @@ export class PostsService {
   constructor(private http: HttpClient) {}
 
   getPosts() {
-    this.http.get<{ id: null, message: string, posts: Post[] }>('http://localhost:3000/api/posts')
-      .subscribe((postData) => {
-        this.posts = postData.posts;
+    this.http
+      .get<{ message: string; posts: any }>(
+        'http://localhost:3000/api/posts'
+      )
+      .pipe(map((postData) => {
+        return postData.posts.map(post => {
+          return {
+            title: post.title,
+            content: post.content,
+            id: post._id
+          }
+        });
+      }))
+      .subscribe((transformedPosts) => {
+        this.posts = transformedPosts;
         this.postUpdated.next([...this.posts]);
       });
   }
@@ -25,13 +38,12 @@ export class PostsService {
 
   addPost(id: string, title: string, content: string) {
     const post: Post = { id: id, title: title, content: content };
-    this.http.post<{message: string}>('http://localhost:3000/api/posts', post)
+    this.http
+      .post<{ message: string }>('http://localhost:3000/api/posts', post)
       .subscribe((responseData) => {
         console.log(responseData.message);
         this.posts.push(post);
         this.postUpdated.next([...this.posts]);
       });
-
   }
-
 }
