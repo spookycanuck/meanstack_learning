8d7e1bedb29eee46d3e2434a31b0a1ddb36e6b06
EM <eric.mann2393@gmail.com>
2021-12-07 17:25:24 -0500

    add image upload form functionality & image preview

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -16,6 +16,7 @@ export class PostCreateComponent implements OnInit {
   post: Post;
   isLoading = false;
   form: FormGroup;
+  imgPreview: string;
   private mode = 'create';
   private postId: string;
 
@@ -27,6 +28,9 @@ export class PostCreateComponent implements OnInit {
         {validators: [Validators.required, Validators.minLength(3)]
         }),
       'content': new FormControl(null,
+        {validators: [Validators.required]
+        }),
+      'image': new FormControl(null,
         {validators: [Validators.required]
         })
     });
@@ -51,6 +55,19 @@ export class PostCreateComponent implements OnInit {
         this.postId = null;
       }
     });
+  }
+
+  onImagePicked(event: Event) {
+    const file = (event.target as HTMLInputElement).files[0];
+    this.form.patchValue({image: file});
+    this.form.get('image').updateValueAndValidity();
+    // console.log(file);
+    // console.log(this.form);
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imgPreview = reader.result as string;
+    }
+    reader.readAsDataURL(file);
   }
 
   onSavePost() {
