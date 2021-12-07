5fbc6ba68f4b7f5f307a92b746918c24d61170d4
EM <eric.mann2393@gmail.com>
2021-12-07 11:05:28 -0500

    add automatic nagivate on post add/update

--- src/app/posts/post.service.ts
+++ src/app/posts/post.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 import { Subject } from 'rxjs';
 import { map } from 'rxjs/operators';
 
@@ -10,7 +11,7 @@ export class PostsService {
   private posts: Post[] = [];
   private postUpdated = new Subject<Post[]>();
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private router: Router) {}
 
   getPosts() {
     this.http
@@ -50,6 +51,7 @@ export class PostsService {
         post.id = id;
         this.posts.push(post);
         this.postUpdated.next([...this.posts]);
+        this.router.navigate(["/"]);
       });
   }
 
@@ -62,6 +64,7 @@ export class PostsService {
         updatedPosts[oldPostIndex] = post;
         this.posts = updatedPosts;
         this.postUpdated.next([...this.posts]);
+        this.router.navigate(["/"]);
       });
   }
 
