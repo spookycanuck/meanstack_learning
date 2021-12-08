9b65f4b0c8b268789480c5140e1b04d85cb2ca50
EM <eric.mann2393@gmail.com>
2021-12-08 15:29:20 -0500

    add imagePath variable to all new post creations

--- src/app/posts/post.service.ts
+++ src/app/posts/post.service.ts
@@ -23,7 +23,8 @@ export class PostsService {
           return {
             title: post.title,
             content: post.content,
-            id: post._id
+            id: post._id,
+            imagePath: post.imagePath
           }
         });
       }))
@@ -48,14 +49,19 @@ export class PostsService {
     postData.append('content', content);
     postData.append('image', image, title);
     this.http
-      .post<{ message: string, postId: string }>(
+      .post<{ message: string, post: Post }>(
         'http://localhost:3000/api/posts',
         postData
       )
       .subscribe((responseData) => {
-        const post: Post = {id: responseData.postId, title: title, content: content}
-        const id = responseData.postId;
-        post.id = id;
+        const post: Post = {
+          id: responseData.post.id,
+          title: title,
+          content: content,
+          imagePath: responseData.post.imagePath
+        };
+        // const id = responseData.post;
+        // post.id = id;
         this.posts.push(post);
         this.postUpdated.next([...this.posts]);
         this.router.navigate(["/"]);
@@ -63,7 +69,7 @@ export class PostsService {
   }
 
   updatePost(id:string, title: string, content: string) {
-    const post: Post = { id: id, title: title, content: content };
+    const post: Post = { id: id, title: title, content: content, imagePath: null };
     this.http.put("http://localhost:3000/api/posts/" + id, post)
       .subscribe(response => {
         const updatedPosts = [...this.posts];
