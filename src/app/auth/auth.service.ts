7074f537e34997a9ae2152c4f40001d8de29e51d
EM <eric.mann2393@gmail.com>
2021-12-14 16:27:02 -0500

    add route to / when login/logout

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from "@angular/common/http";
 
 import { AuthData } from "./auth-data.model";
 import { Subject } from "rxjs";
+import { Router } from "@angular/router";
 
 @Injectable({ providedIn: "root" })
 export class AuthService {
@@ -10,7 +11,7 @@ export class AuthService {
   private token: string;
   private authStatusListener = new Subject<boolean>();
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private router: Router) {}
 
   getToken() {
     return this.token;
@@ -41,8 +42,16 @@ export class AuthService {
         if (token) {
           this.isAuthenticated = true;
           this.authStatusListener.next(true);
+          this.router.navigate(['/']);
         }
 
       })
+  }
+
+  logout() {
+    this.token = null;
+    this.isAuthenticated = false;
+    this. authStatusListener.next(false);
+    this.router.navigate(['/']);
   }
 }
