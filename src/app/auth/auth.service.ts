5166c77c1df1b9d846e2ab46516a94e50cd5e4dd
EM <eric.mann2393@gmail.com>
2021-12-14 16:57:01 -0500

    add session timer for token on backend

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -9,6 +9,7 @@ import { Router } from "@angular/router";
 export class AuthService {
   private isAuthenticated = false;
   private token: string;
+  private tokenTimer: any;
   private authStatusListener = new Subject<boolean>();
 
   constructor(private http: HttpClient, private router: Router) {}
@@ -35,23 +36,28 @@ export class AuthService {
 
   login(email: string, password: string) {
     const authData: AuthData = { email: email, password: password };
-    this.http.post<{token: string}>("http://localhost:3000/api/user/login", authData)
+    this.http.post<{token: string, expiresIn: number}>("http://localhost:3000/api/user/login", authData)
       .subscribe(response => {
         const token = response.token;
         this.token = token;
         if (token) {
+          const expiresIn = response.expiresIn;
+          // console.log(expiresIn);
+          this.tokenTimer = setTimeout(() => {
+            this.logout();
+          }, expiresIn * 1000);
           this.isAuthenticated = true;
           this.authStatusListener.next(true);
           this.router.navigate(['/']);
         }
-
-      })
+      });
   }
 
   logout() {
     this.token = null;
     this.isAuthenticated = false;
     this. authStatusListener.next(false);
+    clearTimeout(this.tokenTimer);
     this.router.navigate(['/']);
   }
 }
